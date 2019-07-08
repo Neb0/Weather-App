ddbe5a29fde1a69a055a8d6c267a1d0fcbc94fb5
Bohdan Bieliaiev <bbieliaiev@gmail.com>
2019-07-08 21:04:43 +0300

    Complete UI, Model; Install Pod files

---
+++ Weather App/Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Bohdan Bieliaiev on 7/8/19.
+//  Copyright © 2019 Bohdan Bieliaiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //linked IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather App/Model/WeatherDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDataModel.swift
+//  Weather App
+//
+//  Created by Bohdan Bieliaiev on 7/8/19.
+//  Copyright © 2019 Bohdan Bieliaiev. All rights reserved.
+//
+
+import Foundation
